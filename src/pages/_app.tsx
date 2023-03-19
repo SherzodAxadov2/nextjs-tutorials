254531a7eeae333b97fee36f3b0d809fa0d437b0
SherzodAxadov2 <sherzodaxadov69@gmail.com>
2023-03-19 20:24:58 +0500

    file structure, mui integration

---
+++ src/pages/_app.tsx
@@ -0,0 +1,29 @@
+import 'src/styles/globals.css'
+import type { AppProps } from 'next/app'
+import { CacheProvider, EmotionCache } from '@emotion/react'
+import createEmotionCache from '../helpers/create-emotion-cache'
+import Head from 'next/head'
+import { ThemeProvider } from '@mui/material/styles'
+import theme from '../helpers/theme'
+import {CssBaseline} from '@mui/material'
+
+const clientSideEmotionCache = createEmotionCache();
+
+export interface MyAppProps extends AppProps{
+  emotionCache?: EmotionCache;
+}
+
+export default function App(props: MyAppProps) {
+  const { Component, emotionCache = clientSideEmotionCache, pageProps } = props;
+
+  return <CacheProvider value={emotionCache}>
+      <Head>
+        <meta name="viewport" content="initial-scale=1, width=device-width" />
+      </Head>
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+          <Component {...pageProps} />
+      </ThemeProvider>
+
+  </CacheProvider> 
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,16 @@
+import Head from "next/head"
+import {Button} from '@mui/material'
+
+const IndexPage = () => {
+  return (
+    <>
+      <Head>
+        <title>Sammi app</title>
+      </Head>
+      <div>IndexPage</div>
+      <Button>Click</Button>
+    </>
+  )
+}
+
+export default IndexPage
